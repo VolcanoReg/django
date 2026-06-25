7ea9045f179a90eaf74b978409eb224778c38a58
VolcanoReg <90879632+VolcanoReg@users.noreply.github.com>
2026-06-25 15:52:52 +0700

    feat: Memperbaiki Core tidak terdaftar di INSTALLED_APPS di django

---
+++ code/simplelms/local_settings.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+settings.INSTALLED_APPS += ['core']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'simple_lms',
+        'USER': 'simple_user',
+        'PASSWORD': 'simple_password',
+        'HOST': 'simple_db',
+    }
+}
